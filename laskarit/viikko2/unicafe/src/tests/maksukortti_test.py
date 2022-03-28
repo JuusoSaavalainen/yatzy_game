2700ad49cdcb00674307c8d1ba47487e4a447e6e
Juuso <juuso.saavalainen@helsinki.fi>
2022-03-28 21:33:37 +0300

    lisää ei commitattua vk2

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,18 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_saldo_oikein(self):
+        self.assertEqual(str(self.maksukortti), "saldo: 0.1")
+
+    def test_lataus_onnistuu(self):
+        self.maksukortti.lataa_rahaa(10)
+        self.assertEqual(str(self.maksukortti), "saldo: 0.2")
+    
+    def test_rahan_ottaminen_toimii(self):
+        x = self.maksukortti.ota_rahaa(15)
+        y = self.maksukortti.ota_rahaa(9)
+        self.assertEqual(x,False)
+        self.assertEqual(y,True)
+        self.assertEqual(str(self.maksukortti), "saldo: 0.01")
+        
